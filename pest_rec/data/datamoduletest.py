c62eda5fc32ccb5f0ae4d68a1b3266f853ec6c73
chmpgnesupernova <101654598+chmpgnesupernova@users.noreply.github.com>
2025-05-10 05:44:55 +0900

    erase

--- pest_rec/data/datamoduletest.py
+++
@@ -1,8 +0,0 @@
-from ip102_datamodule import IP102DataModule
-
-dm = IP102DataModule(batch_size=32)
-# dm.prepare_data()
-dm.setup()
-feature, target = next(iter(dm.train_dataloader()))
-
-print(feature.shape)
